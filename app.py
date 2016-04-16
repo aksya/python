61bde1057368e51a4bc05ae563e16d66eb8903ec
aksya <aksya82@mail.ru>
2016-04-16 05:51:49 +0000

    16_04_16

--- app.py
+++ app.py
@@ -1,37 +1,30 @@
 import sys
 import csv
 
-def day_up_value(list, max_value):
+def day_up_value(close_prices, max_value):
     day = 0
-    for closing_price in list:
-        if closing_price > max_value: 
+    for close_price in close_prices:
+        if close_price > max_value: 
             day = day + 1
     return day
     
 
-def main(file_name,max_value, file_name_print):
+def main(file_name, max_value, file_name_print=None):
     with open(file_name) as f:
         f.readline()
         csv_file = csv.reader(f, delimiter=',')
-        list = []
+        close_prices = []
         for row in csv_file:
-            #print(float((row[4])))
             x=float((row[4]))
-            list.append(x)
-    day = day_up_value(list, max_value)        
-    #day = 0
-    #for closing_price in list:
-    #    if closing_price > max_value: 
-    #        day = day + 1
-    if file_name_print=="":
-        print(day)
-    else:
-        f_out = open(file_name_print, 'w')
-        f_out.write(str(day))
-        print("В файл")
-        
-              
-            
+            close_prices.append(x)
+    day = day_up_value(close_prices, max_value)        
+    if file_name_print:
+        with open(file_name_print, 'w') as f_out:
+            f_out.write(str(day))
+
+        #f_out = open(file_name_print, 'w')
+        #f_out.write(str(day))
+    print(day)
 
 
 if __name__ == '__main__':
@@ -39,10 +32,8 @@ if __name__ == '__main__':
         file_name = sys.argv[1]
         if len(sys.argv) > 2:
             max_value = sys.argv[2]
-            if len(sys.argv) > 3:
-                main(file_name,float(max_value),sys.argv[3])
-            else:    
-                main(file_name,float(max_value),"")
+            file_name_print = sys.argv[3] if len(sys.argv) > 3 else None
+            main(file_name, float(max_value), file_name_print)
         else:
             print("Укажите уровень закрытия цены")
     else:    
