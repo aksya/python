336cdd6fe0434fdbb0d8a7b07e5b45e20d100c4b
aksya <aksya82@mail.ru>
2016-05-07 05:46:56 +0000

    07-05-16

--- app.py
+++ app.py
@@ -1,40 +1,36 @@
 import sys
-import csv
+import argparse
+import process
+import logging
 
-def day_up_value(close_prices, max_value):
-    day = 0
-    for close_price in close_prices:
-        if close_price > max_value: 
-            day = day + 1
-    return day
-    
-
-def main(file_name, max_value, file_name_print=None):
-    with open(file_name) as f:
-        f.readline()
-        csv_file = csv.reader(f, delimiter=',')
-        close_prices = []
-        for row in csv_file:
-            x=float((row[4]))
-            close_prices.append(x)
-    day = day_up_value(close_prices, max_value)        
-    if file_name_print:
-        with open(file_name_print, 'w') as f_out:
-            f_out.write(str(day))
-
-        #f_out = open(file_name_print, 'w')
-        #f_out.write(str(day))
-    print(day)
-
+ 
+def createParser ():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--symbol", help="Биржевой символ акций")
+    parser.add_argument("-f","--file", help="Файл для обработки")
+    parser.add_argument ("up_value",  help="Максимальный уровень цен", type=int)
+    parser.add_argument ("-y", "--year", help="Год", default=2016, type=int)
+    parser.add_argument ("-fo", "--file_out", help="Файл для записи результата", default="rezult.txt")
+    parser.add_argument ("-fl", "--file_log", help="Файл для логирования", default="app.log")
+    parser.add_argument ("-l", "--level_log", help="Уровень логирования", default="INFO")
+ 
+    return parser
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        file_name = sys.argv[1]
-        if len(sys.argv) > 2:
-            max_value = sys.argv[2]
-            file_name_print = sys.argv[3] if len(sys.argv) > 3 else None
-            main(file_name, float(max_value), file_name_print)
-        else:
-            print("Укажите уровень закрытия цены")
-    else:    
-        print("Укажите имя файла")
+    parser = createParser()
+    namespace = parser.parse_args(sys.argv[1:])
+    if namespace.file:
+        logging.basicConfig(filename=namespace.file_log, level=namespace.level_log)
+        logging.info('Программа запущена с параметрами ' + str(namespace) + "в режиме файл")
+        
+        try:
+            process.process_file(namespace.file, namespace.up_value, namespace.file_out, namespace.file_log, namespace.level_log) 
+        except Exception as e:
+            logging.info(str(e))
+    elif namespace.symbol:
+        logging.basicConfig(filename=namespace.file_log, level=namespace.level_log)
+        logging.info('Программа запущена с параметрами ' + str(namespace) + "в режиме сеть")
+        try:
+            process.process_network(namespace.symbol, namespace.up_value, namespace.year,  namespace.file_out, namespace.file_log, namespace.level_log)
+        except Exception as e:
+            logging.info(str(e))
